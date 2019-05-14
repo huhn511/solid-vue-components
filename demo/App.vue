6500f27cef0617bf75e60db3cbebf2b5e056d204
huhn <sehe89@gmail.com>
2019-05-14 23:18:49 +0200

    add solid login and logout button

--- demo/App.vue
+++ demo/App.vue
@@ -1,5 +1,34 @@
 <template>
   <div id="app">
-    <LoginButton />
+    <LoginButton v-on:onSuccess="handleLogin" v-if="!session" />
+    <LogoutButton v-on:onSuccess="handleLogout" v-if="session" />
   </div>
 </template>
+
+<script>
+import auth from 'solid-auth-client'
+
+export default {
+  data() {
+    return {
+      session: false
+    }
+  },
+  created() {
+    auth.currentSession().then(session => {
+      console.log("session", session)
+      this.session = session;
+    })
+  },
+  methods: {
+    handleLogin(session) {
+      console.log("handleLogin", session)
+      this.session = session;
+    },
+    handleLogout() {
+      console.log("handleLogout")
+      this.session = false
+    }
+  }
+}
+</script>
