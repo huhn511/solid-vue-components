e11f1b7931e23575978f6a6839b8852f5c5dfc90
huhn <sehe89@gmail.com>
2019-05-15 21:45:22 +0200

    add AuhButton, LoggedIn and LoggedOut component

--- demo/App.vue
+++ demo/App.vue
@@ -1,34 +1,19 @@
 <template>
   <div id="app">
-    <LoginButton v-on:onSuccess="handleLogin" v-if="!session" />
-    <LogoutButton v-on:onSuccess="handleLogout" v-if="session" />
+    <AuthButton />
+    <LoggedIn>
+      <p>This is only visible for logged in users.</p>
+    </LoggedIn>
+    <LoggedOut>
+      <p>This is only visible for logged out users.</p>
+    </LoggedOut>
   </div>
 </template>
 
 <script>
 import auth from 'solid-auth-client'
 
+
 export default {
-  data() {
-    return {
-      session: false
-    }
-  },
-  created() {
-    auth.currentSession().then(session => {
-      console.log("session", session)
-      this.session = session;
-    })
-  },
-  methods: {
-    handleLogin(session) {
-      console.log("handleLogin", session)
-      this.session = session;
-    },
-    handleLogout() {
-      console.log("handleLogout")
-      this.session = false
-    }
-  }
 }
 </script>
---
+++ src/LoggedOut.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <slot v-if="!webId"></slot>
+  </div>
+</template>
+
+<script>
+import auth from 'solid-auth-client'
+
+export default {
+  data() {
+    return {
+      webId: null
+    }
+  },
+  mounted() {
+    auth.trackSession(session => {
+      this.webId = session ? session.webId : null
+    })
+  }
+}
+</script>
+
+
+<meta>
+{
+  "description": "Pane that only shows its contents when the user is logged in."
+}
+</meta>
+
+<example>
+    <LoggedIn>
+      <p>This is only visible for logged in users.</p>
+    </LoggedIn>
+</example>
